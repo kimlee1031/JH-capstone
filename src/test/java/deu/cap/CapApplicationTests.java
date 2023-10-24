5b0907b9027007a30fdfbe0240f3c22f215ecba4
kimlee1031 <kwhazard0@gmail.com>
2023-10-24 17:20:32 +0900

    jh capstone

---
+++ src/test/java/deu/cap/CapApplicationTests.java
@@ -0,0 +1,13 @@
+package deu.cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
